f8e682f31e76c9d55c18ac89134c146de623b8e4
amin mousavi <mousavia54@gmail.com>
2023-12-12 13:21:04 +0330

    Define Postgresql as database

--- DjangoCRM/settings.py
+++ DjangoCRM/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'DjangoCRM.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangocrm',
+        'USER': 'postgres',
+        'PASSWORD': 'hamilton23',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
