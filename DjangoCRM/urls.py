e555c03472fcb9cc5d3e81ee26f3d348c3c5d87b
amin mousavi <mousavia54@gmail.com>
2023-12-12 14:40:40 +0330

    Create Login page

--- DjangoCRM/urls.py
+++ DjangoCRM/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from DjangoCRM.apps.mycrm import urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('mycrm/', include('DjangoCRM.apps.mycrm.urls'))
 ]
