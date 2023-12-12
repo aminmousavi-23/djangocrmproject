c03075165a87776af538d2f0581e26ea7d46529c
amin mousavi <mousavia54@gmail.com>
2023-12-12 16:43:20 +0330

    Create Record model and register in admin

--- DjangoCRM/apps/mycrm/admin.py
+++ DjangoCRM/apps/mycrm/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from DjangoCRM.apps.mycrm.models import Record
 
-# Register your models here.
+class RecordAdmin(admin.ModelAdmin):
+    list_display = ['created_at','first_name','last_name','email','phone','city','state']
+
+admin.site.register(Record, RecordAdmin)
